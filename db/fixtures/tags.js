64e6fdb80a1eb19a12822e3693f59b76e5be31de
Greg Kops <greg@thinktopography.com>
2017-04-03 00:30:19 -0400

    migrating cms to new repository

---
+++ db/fixtures/tags.js
@@ -0,0 +1,29 @@
+module.exports = {
+  tableName: "cms_tags",
+  records: [
+    {
+      id: 1,
+      team_id: 1,
+      website_id: 1,
+      name: 'one',
+      created_at: "2017-03-23T15:18:41.134Z",
+      updated_at: "2017-03-23T15:18:41.135Z"
+    },
+    {
+      id: 2,
+      team_id: 1,
+      website_id: 1,
+      name: 'two',
+      created_at: "2017-03-23T15:18:41.134Z",
+      updated_at: "2017-03-23T15:18:41.135Z"
+    },
+    {
+      id: 3,
+      team_id: 1,
+      website_id: 1,
+      name: 'three',
+      created_at: "2017-03-23T15:18:41.134Z",
+      updated_at: "2017-03-23T15:18:41.135Z"
+    }
+  ]
+}
